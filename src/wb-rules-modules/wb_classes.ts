1cdc9f42aef0beecf247c64cddb4a97753556fbe
Maxim Rydikov <rydikov@gmail.com>
2025-10-13 18:50:12 +0300

    refactor

--- src/wb-rules-modules/wb_classes.ts
+++ src/wb-rules-modules/wb_classes.ts
@@ -12,14 +12,14 @@ class BaseDevice {
     return this.control?.getValue()
   }
 
+  set_value(value: string | number | boolean) {
+    this.control?.setValue(value)
+  }
+
 }
 
 // Свет включающийся через реле
 export class RelayLight extends BaseDevice {
-
-  set_value(value: string | number | boolean) {
-    this.control?.setValue(value)
-  }
 
   on() {
     this.set_value(true)
--- src/wb-rules/ax_pro_arm.ts
+++ src/wb-rules/ax_pro_arm.ts
@@ -1,12 +1,11 @@
-import { AxProAreas, axProStatesEnum } from '@wbm/global_devices'
-import { RelayLight } from '@wbm/wb_classes'
+import { AxProAreas, axProStatesEnum, RelayLights } from '@wbm/global_devices'
 
 defineRule('ArmGroundFloor', {
   whenChanged: AxProAreas['GroundFloor'],
   then: function (newValue: axProStatesEnum) {
     if (newValue === axProStatesEnum.Armed) {
       log.debug('Подвал поставлен на охрану')
-      new RelayLight('Сabinet_01').off()
+      RelayLights.Сabinet_01.off()
     }
   },
 })
